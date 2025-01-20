f93b3370da028eb63cba17de550cf2b27335d654
Carlos Nexans <carlos.nexans@clau.com>
2025-01-20 12:44:33 -0300

    add mixed operations jit test

--- test/compiler_test.py
+++ test/compiler_test.py
@@ -5,7 +5,7 @@ from src.token import Token, TokenType
 from src.ast import Program, Assignment, Identifier, Integer, PlusOperation
 
 class TestCompiler(TestCase):
-    def test_compiler_success(self):
+    def test_assign_global_variable(self):
         # Create module and compile
         src = "x = 1 + 2"
         compiler = Compiler(src)
@@ -17,3 +17,16 @@ class TestCompiler(TestCase):
         # x should be 3
         actual_value = compiler.get_symbol_value(engine, "x")
         self.assertEqual(3, actual_value)
+
+    def test_mixed_operations(self):
+        # Create module and compile
+        src = "x = 1 + 2 * 3 + 5 / 5"
+        compiler = Compiler(src)
+        result, engine = compiler.execute_jit()
+
+        # Main method should return 0
+        self.assertEqual(result, 0)
+
+        # x should be 3
+        actual_value = compiler.get_symbol_value(engine, "x")
+        self.assertEqual(8, actual_value)
